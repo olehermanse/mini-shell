0350fb0af3e2a0afe5c5b31af57cb6ae4f750d74
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2016-08-06 20:14:16 +0200

    Switched to strtok for parsing.

--- src/mini-shell.c
+++ src/mini-shell.c
@@ -1,54 +1,6 @@
 #include "mini-shell.h"
 
-char** parseLine(char* buffer){
-    int index = 0;
-    char c = buffer[index];
-
-    while(isspace(c) && c != 0){
-        c = buffer[++index];
-    }
-    if(c == 0){
-        return 0;
-    }
-
-    typedef enum {CHARACTER, WHITESPACE} parseState;
-    parseState state = CHARACTER;
-
-    const int MAX_WORDS = 32;
-    char* words[MAX_WORDS+1];
-    int wordsCounter = 0;
-
-    char word[128];
-    char wordIndex = 0;
-    while(c != 0 && wordsCounter <= MAX_WORDS){
-        if(state == CHARACTER){
-            if(isspace(c)){
-                state = WHITESPACE;
-                word[wordIndex++] = 0;
-                words[wordsCounter] = (char*)(malloc(wordIndex));
-                strcpy(words[wordsCounter], word);
-                ++wordsCounter;
-                wordIndex = 0;
-            }else{
-                word[wordIndex++] = c;
-            }
-        }else if(state == WHITESPACE){
-            if(!isspace(c)){
-                state = CHARACTER;
-                word[wordIndex++] = c;
-            }
-        }
-        c = buffer[++index];
-    }
-    char** returnWords = (char**)(malloc( (wordsCounter+1) * sizeof(char*) ));
-    returnWords[wordsCounter] = NULL;
-    for(int i = 0; i<wordsCounter; ++i){
-        returnWords[i] = words[i];
-    }
-    return returnWords;
-}
-
-void ** nestedFree(void ** pointerArray){
+void ** freeArray(void ** pointerArray){
     assert(pointerArray);
     for( int i = 0; pointerArray[i] != 0; ++i){
         free(pointerArray[i]);
@@ -57,6 +9,40 @@ void ** nestedFree(void ** pointerArray){
     free(pointerArray);
     pointerArray = 0;
     return pointerArray;
+}
+
+char** strcpyArray(char** strings){
+    int count = 0;
+    while(strings[count++]);
+    char** newArray = malloc(sizeof(char*)*count);
+    count = 0;
+    while(strings[count]){
+        newArray[count] = malloc(sizeof(char)*(1+strlen(strings[count])));
+        strcpy(newArray[count], strings[count]);
+        ++count;
+    }
+    newArray[count] = 0;
+    return newArray;
+}
+
+char** parseString(char* buffer){
+    char* tokens[32];
+    int index = 0;
+    const char sep[] = " \t\n\v\f\r";
+    char* token = strtok(buffer, sep);
+    while(token){
+        tokens[index++] = token;
+        token = strtok(NULL, sep);
+    }
+    tokens[index++] = NULL;
+    char** returnTokens = malloc(sizeof(char*)*index);
+    index = 0;
+    while(tokens[index]){
+        returnTokens[index] = tokens[index];
+        ++index;
+    }
+    returnTokens[index] = NULL;
+    return returnTokens;
 }
 
 int main(int argc, char** argv){
@@ -72,15 +58,14 @@ int main(int argc, char** argv){
     strcpy(user, buffer);
 
     while(result){
-        printf("%s@mini-shell %i> ", user, cmdCounter);   // Print standard line beginning
+        printf("%s@mini-shell %i> ", user, cmdCounter);
         result = (long)fgets(buffer, bufferSize, stdin);
-        //Only execute and save lines that are not empty
         if(buffer[0] != 0){
-            char** words = parseLine(buffer);
+            char** words = parseString(buffer);
             for(int i = 0; words[i] != 0; ++i){
                 printf("'%s'\n", words[i]);
             }
-            nestedFree((void**)(words));
+            free((void**)(words));
             result = 1;
         }
         ++cmdCounter;
